391b79d11a1104f98d625cf22701d1eb009fe256
Howe Yang <howecyang@gmail.com>
2019-07-13 21:37:38 -0400

    wtf..
    ok

---
+++ build/precache-manifest.6ba1427f987c8dcf4003ee81e901ebcb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4d9b658bbc1fbf5d7cd09e4daec383",
+    "url": "/howeyang.github.io/index.html"
+  },
+  {
+    "revision": "45f6ceef5a5dd99ab4fa",
+    "url": "/howeyang.github.io/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "6a53fd4cc57d769a7e4d",
+    "url": "/howeyang.github.io/static/css/main.7e965200.chunk.css"
+  },
+  {
+    "revision": "45f6ceef5a5dd99ab4fa",
+    "url": "/howeyang.github.io/static/js/2.54176f56.chunk.js"
+  },
+  {
+    "revision": "6a53fd4cc57d769a7e4d",
+    "url": "/howeyang.github.io/static/js/main.bd668ce9.chunk.js"
+  },
+  {
+    "revision": "88d7778b6a635c03ba06",
+    "url": "/howeyang.github.io/static/js/runtime~main.3474ee5f.js"
+  },
+  {
+    "revision": "4aac14a393e8401bda5aea0592d3c5df",
+    "url": "/howeyang.github.io/static/media/color1.4aac14a3.png"
+  },
+  {
+    "revision": "8de1fb8aaf840a837958e195b62ba54a",
+    "url": "/howeyang.github.io/static/media/color11.8de1fb8a.png"
+  },
+  {
+    "revision": "23ac3289ca1fac6a0585667222bd6f40",
+    "url": "/howeyang.github.io/static/media/color12.23ac3289.png"
+  },
+  {
+    "revision": "5807ec1cbe7f78e996d936f2e665a1bd",
+    "url": "/howeyang.github.io/static/media/color13.5807ec1c.png"
+  },
+  {
+    "revision": "d4ca54059330e69fc6c1737fdb5ccfb5",
+    "url": "/howeyang.github.io/static/media/color4.d4ca5405.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1dd483c77c59454f37d4282e583b554c.js"
+  "/howeyang.github.io/precache-manifest.6ba1427f987c8dcf4003ee81e901ebcb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/howeyang.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
