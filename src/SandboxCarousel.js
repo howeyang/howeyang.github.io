ead6fd6771c3070fc844241c000e661d307499c6
Howe Yang <howecyang@gmail.com>
2019-08-14 19:59:31 -0400

    Awesomeslider + ReactPlayer
    = working player

--- src/SandboxCarousel.js
+++ src/SandboxCarousel.js
@@ -1,19 +1,37 @@
 import React, { Component } from "react";
-import AwesomeSlider from 'react-awesome-slider';
-import 'react-awesome-slider/dist/styles.css';
+import AwesomeSlider from "react-awesome-slider";
+import ReactPlayer from "react-player";
+import "react-awesome-slider/dist/styles.css";
 
 //https://www.npmjs.com/package/react-responsive-carousel
 class SandboxCarousel extends Component {
   render() {
     return (
-      <AwesomeSlider >
-        <div data-src="./Sandbox/ps4_wave_experiment2.gif.mp4"  allowfullscreen loop="true" autoplay="autoplay" controls="controls" id="vid" muted type="video/mp4">
-        <p className="highz">Scrolling Texture</p>
+      <AwesomeSlider>
+        <div>
+          <ReactPlayer
+            url="./Sandbox/ps4_wave_experiment2.gif.mp4"
+            width="100%"
+            height="100%"
+            controls={true}
+            playing={true}
+            loop={true}
+            muted={true}
+          />
+          <p className="highz">Scrolling Texture</p>
         </div>
-        
-        <div data-src="./Sandbox/ps4_wave_experiment.gif.mp4"  allowfullscreen loop="true" autoplay="autoplay" controls="controls" id="vid" muted type="video/mp4">
-        
-        <p className="highz">Mesh Distortion </p>
+
+        <div>
+          <ReactPlayer
+            url="./Sandbox/ps4_wave_experiment.gif.mp4"
+            width="100%"
+            height="100%"
+            controls={true}
+            playing={true}
+            loop={true}
+            muted={true}
+          />
+          <p className="highz">Mesh Distortion</p>
         </div>
       </AwesomeSlider>
     );
@@ -21,3 +39,10 @@ class SandboxCarousel extends Component {
 }
 
 export default SandboxCarousel;
+/* <div data-src="./Sandbox/ps4_wave_experiment.gif.mp4"  allowfullscreen loop="true" autoplay="autoplay" controls="controls" id="vid" muted type="video/mp4">
+
+<div> <video src="./Sandbox/ps4_wave_experiment.gif.mp4"  loop="loop" autoPlay="autoPlay"  controls id="vid" muted type="video/mp4" />
+        <p className="highz">Mesh Distortion </p>
+        </div>
+
+*/
