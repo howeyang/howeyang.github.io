4d1761ebd7b3aa4d02fe4f7c486fb1af87259bd8
Howe Yang <howecyang@gmail.com>
2019-11-23 11:44:04 -0500

    Small changes to background vid
    x

---
+++ src/Components/BackgroundVideo.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class BackgroundVideo extends Component {
+
+  render() {
+    return (
+      <video loop muted autoPlay poster="black" className="fullscreen-bg">
+        <source src="beachwave2.mp4" type="video/mp4" />
+      </video>
+    );
+  }
+}
+
+export default BackgroundVideo;
