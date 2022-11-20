bee2159070649daae65798c5c75ba2c9714ab27b
Howe Yang <howe.c.yang@gmail.com>
2022-11-19 19:26:55 -0500

    fixing safari bug and working on revamp

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -11,9 +11,30 @@ class LandingPage extends Component {
     window.scrollTo(0, 0);
   }
 
+  
+
   render() {
     return (
       <div>
+        <div>
+        <div className="sidewaywrapper">
+              <div className="sidewayscrolling">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                  volutpat, ante eu bibendum tincidunt, sem lacus vehicula
+                  augue, ut suscipit.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                  volutpat, ante eu bibendum tincidunt, sem lacus vehicula
+                  augue, ut suscipit.
+                </p>
+              </div>
+            </div>
+          <div className="sidenav">
+            <div className="sidewaytext">Howe Yang</div>
+          </div>
+        </div>
         <div className="grid_root">
           <Grid
             container
@@ -22,40 +43,32 @@ class LandingPage extends Component {
             alignItems="stretch"
             spacing={2}
           >
-            <Grid item xs={12} sm={12} md={12} className="overlay2 nameland">
-              <Fade duration={2000}>
-                <div>
-                  <div className="white0 mobilepad"> Howe Yang </div>
+            <Grid item xs={12} sm={12} md={12}>
+              <Fade duration={2000}></Fade>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12}>
+              <Fade delay={300}>
+                <p className="black2 mobilepad">
+                  <label className="blackbutton">
+                    Obssessed with making things look good!
+                  </label>
+                </p>
+              </Fade>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12}>
+              <Fade bottom duration={1250}>
+                <div className="white2 mobilepad">
+                  {" "}
+                  Unity3D Developer | Learning VFX
                 </div>
               </Fade>
-              </Grid>
-              <Grid item xs={12} sm={12} md={12}>
-                <Fade delay={300}>
-                  <p className="black2 mobilepad">
-                    <label className="blackbutton">
-                      Obssessed with making things look good!
-                    </label>
-                  </p>
-                </Fade>
-              </Grid>
-              <Grid item xs={12} sm={12} md={12}>
-                <Fade bottom duration={1250}>
-                  <div className="white2 mobilepad">
-                    {" "}
-                    Unity3D Developer | Learning VFX
-                  </div>
-                </Fade>
-              </Grid>
-            
+            </Grid>
 
             <Grid item xs={12} sm={6} md={3}>
               <CardExperience delay={100} />
             </Grid>
             <Grid item xs={12} sm={6} md={3}>
               <CardProjects delay={250} />
-            </Grid>
-            <Grid item xs={12} sm={6} md={3}>
-              <CardAutobattler delay={400} />
             </Grid>
           </Grid>
         </div>
