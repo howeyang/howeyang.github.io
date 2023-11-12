2b7f70ca6202a83e5a53de803023e6665a278b5d
Howe Yang <howe.c.yang@gmail.com>
2023-11-12 11:31:02 -0500

    Set up Divide and working between Transitions

---
+++ src/Pages/LeftLandingPage.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import CardExperience from "../Cards/CardExperience";
+import CardAutobattler from "../Cards/CardAutobattler";
+import CardProjects from "../Cards/CardProjects";
+import Fade from "react-reveal/Fade";
+import { withRouter } from "react-router-dom";
+
+class LandingPage extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    return (
+      <div>
+        <Fade duration={2000}>
+          <div>
+            <div className=""> Left Container </div>
+          </div>
+        </Fade>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Route, NavLink, HashRouter, Switch } from "react-router-dom";
 import LandingPage from "./LandingPage";
+import LeftLandingPage from "./LeftLandingPage";
 import ProjectPage from "./ProjectPage";
 import ExperiencePage from "./ExperiencePage";
 import ContactPage from "./ContactPage";
@@ -13,24 +14,23 @@ import ProjectReact from "../Projects/ProjectReact";
 import ProjectBeachWave from "../Projects/ProjectBeachWave";
 import ProjectFire from "../Projects/ProjectFire";
 import NoMatch from "./NoMatch";
-
+import Grid from "@material-ui/core/Grid";
 
 class Main extends Component {
-
   state = {
-    class: 'header invisiblenav'
-  }
+    class: "header invisiblenav"
+  };
 
   listenScrollEvent = e => {
     if (window.scrollY > 1) {
-      this.setState({class: 'header visiblenav'})
+      this.setState({ class: "header visiblenav" });
     } else {
-      this.setState({class: 'header'})
+      this.setState({ class: "header" });
     }
-  }
+  };
 
   componentDidMount() {
-    window.addEventListener('scroll', this.listenScrollEvent)
+    window.addEventListener("scroll", this.listenScrollEvent);
   }
 
   render() {
@@ -54,25 +54,65 @@ class Main extends Component {
             </li>
           </ul>
         </Hidden>
-        <Hidden smUp> 
-        <MobileBar></MobileBar>
+        <Hidden smUp>
+          <MobileBar></MobileBar>
         </Hidden>
 
-        <div className="content">
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/Project" component={ProjectPage} />
-          <Route exact path="/Experience" component={ExperiencePage} />
-          <Route exact path="/Contact" component={ContactPage} />
-          <Route exact path="/Project/ProjectA" component={ProjectA} />
-          <Route exact path="/Project/ProjectAutobattler" component={ProjectAutoBattler} />
-          <Route exact path="/Project/ProjectSandbox" component={ProjectSandbox} />
-          <Route exact path="/Project/ProjectReact" component={ProjectReact} />
-          <Route exact path="/Project/ProjectBeachWave" component={ProjectBeachWave} />
-          <Route exact path="/Project/ProjectFire" component={ProjectFire} />
-          <Route component={NoMatch} />
-          </Switch>
-        </div>
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-between"
+          alignItems="stretch"
+          className="landingContainer"
+        >
+          <Grid item xs={4} sm={4} md={4} className="leftContainer forceHeight">
+            <LeftLandingPage></LeftLandingPage>
+          </Grid>
+
+          <Grid
+            item
+            xs={8}
+            sm={8}
+            md={8}
+            className="middleContainer forceHeight"
+          >
+            <div className="content">
+              <Switch>
+                <Route exact path="/" component={LandingPage} />
+                <Route exact path="/Project" component={ProjectPage} />
+                <Route exact path="/Experience" component={ExperiencePage} />
+                <Route exact path="/Contact" component={ContactPage} />
+                <Route exact path="/Project/ProjectA" component={ProjectA} />
+                <Route
+                  exact
+                  path="/Project/ProjectAutobattler"
+                  component={ProjectAutoBattler}
+                />
+                <Route
+                  exact
+                  path="/Project/ProjectSandbox"
+                  component={ProjectSandbox}
+                />
+                <Route
+                  exact
+                  path="/Project/ProjectReact"
+                  component={ProjectReact}
+                />
+                <Route
+                  exact
+                  path="/Project/ProjectBeachWave"
+                  component={ProjectBeachWave}
+                />
+                <Route
+                  exact
+                  path="/Project/ProjectFire"
+                  component={ProjectFire}
+                />
+                <Route component={NoMatch} />
+              </Switch>
+            </div>
+          </Grid>
+        </Grid>
         <footer className="footer"></footer>
       </HashRouter>
     );
