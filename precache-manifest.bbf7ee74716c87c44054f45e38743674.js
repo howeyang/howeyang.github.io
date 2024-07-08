688464c7883183bef44619085e5e201c35f7258d
Howe Yang <howe.c.yang@gmail.com>
2024-07-08 15:32:59 -0400

    Updates

---
+++ precache-manifest.bbf7ee74716c87c44054f45e38743674.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e370bce0dee72c3ac892b20e75dae5cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "146ac70dc7bde6088c3f",
+    "url": "/static/css/2.da68fc70.chunk.css"
+  },
+  {
+    "revision": "9d390a1bebedde9dd79c",
+    "url": "/static/css/main.aa8c6b55.chunk.css"
+  },
+  {
+    "revision": "146ac70dc7bde6088c3f",
+    "url": "/static/js/2.3c6bf76e.chunk.js"
+  },
+  {
+    "revision": "9d390a1bebedde9dd79c",
+    "url": "/static/js/main.cc319296.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d6425e115af5dd15dd91d574ed1ae51f",
+    "url": "/static/media/BattleRunMain.d6425e11.png"
+  },
+  {
+    "revision": "4a3e7c251505f8ea9cf5b35576f03354",
+    "url": "/static/media/BattleRunSplash.4a3e7c25.png"
+  },
+  {
+    "revision": "ed70842cb2e56125ae5464b6f5856359",
+    "url": "/static/media/DustSpin.ed70842c.png"
+  },
+  {
+    "revision": "72bbb25eedf7b7c4c9fbab8377498d14",
+    "url": "/static/media/MacieJump.72bbb25e.png"
+  },
+  {
+    "revision": "0bf0549ab31eddaf0ca5679e707bdc97",
+    "url": "/static/media/Splash_BTB4.0bf0549a.jpg"
+  },
+  {
+    "revision": "6faf5e97c60e47e1d8b5de4488877ea7",
+    "url": "/static/media/ThumbBreakdown.6faf5e97.png"
+  },
+  {
+    "revision": "de9cda74e1607c03ee8cfe9fd7720b51",
+    "url": "/static/media/ThumbCozy1.de9cda74.png"
+  },
+  {
+    "revision": "f6a7da6e00a7505c84fb253b4b9b9e23",
+    "url": "/static/media/ThumbCozy2.f6a7da6e.png"
+  },
+  {
+    "revision": "b59cb147ed7e98ce967a88f3c37bdbe1",
+    "url": "/static/media/ThumbFire1.b59cb147.png"
+  },
+  {
+    "revision": "98f37247115e96b252d6c6bb64b8951f",
+    "url": "/static/media/ThumbFire2.98f37247.png"
+  },
+  {
+    "revision": "615f43814a2840346b805cd67b13639a",
+    "url": "/static/media/ThumbGacha.615f4381.png"
+  },
+  {
+    "revision": "7bac66f61352b1220f9e22f60972957c",
+    "url": "/static/media/ThumbLanding.7bac66f6.png"
+  },
+  {
+    "revision": "8d815faef88c3ce345058a80a70b7e77",
+    "url": "/static/media/ThumbLeader.8d815fae.png"
+  },
+  {
+    "revision": "b79b06bce538e1a6c8afb332e13e8033",
+    "url": "/static/media/ThumbSpeed.b79b06bc.png"
+  },
+  {
+    "revision": "96111f0c778dcfc0f681a0bda4b5c17b",
+    "url": "/static/media/ThumbWave.96111f0c.png"
+  },
+  {
+    "revision": "ca3eb194387075be9b294fb41d5aff80",
+    "url": "/static/media/avatar.ca3eb194.gif"
+  },
+  {
+    "revision": "1aa6addfe9f3f9b2b367cdb3520debdf",
+    "url": "/static/media/boss4.1aa6addf.png"
+  },
+  {
+    "revision": "6bf4def8f8ab538051a2744f966f84e5",
+    "url": "/static/media/bossrevamp.6bf4def8.jpg"
+  },
+  {
+    "revision": "efa8c1f6dca0ae922fa33b0301b5b8a4",
+    "url": "/static/media/briefcase.efa8c1f6.svg"
+  },
+  {
+    "revision": "76215a51f5bd5264f09fdbe8cb2b5be6",
+    "url": "/static/media/card-autobattler.76215a51.png"
+  },
+  {
+    "revision": "afd2c9ff3d2ebb8aa976996c39b3f436",
+    "url": "/static/media/card-battlerun.afd2c9ff.png"
+  },
+  {
+    "revision": "396b914baea227081c60d15136e8851e",
+    "url": "/static/media/card-beach.396b914b.png"
+  },
+  {
+    "revision": "b377e419e6af9ba198266fe23f7377f9",
+    "url": "/static/media/card-cozy.b377e419.png"
+  },
+  {
+    "revision": "5fc2e1aa51be60427dd3252a1bb2dd2a",
+    "url": "/static/media/card-galaxy.5fc2e1aa.png"
+  },
+  {
+    "revision": "41bb0e25ae9835d9372917620d5b463d",
+    "url": "/static/media/card-react.41bb0e25.png"
+  },
+  {
+    "revision": "4d640c48427959b320839b2dcf15d103",
+    "url": "/static/media/card-sandbox.4d640c48.png"
+  },
+  {
+    "revision": "ced7eb9aafc6f0b7d37f0e08f79f7000",
+    "url": "/static/media/cozycafe_logo.ced7eb9a.png"
+  },
+  {
+    "revision": "65b13d43acc4aabe1080dc091e3b6d5b",
+    "url": "/static/media/cozycafe_vertical.65b13d43.png"
+  },
+  {
+    "revision": "96c14987b4654cb5a12a2473601afc5f",
+    "url": "/static/media/cozycafebigsplash.96c14987.png"
+  },
+  {
+    "revision": "e10880f84186e307989195f13203e656",
+    "url": "/static/media/cute-hanging.e10880f8.png"
+  },
+  {
+    "revision": "59e6ef030da9a5adf52088253387b203",
+    "url": "/static/media/dashboard1.59e6ef03.png"
+  },
+  {
+    "revision": "1e23ef8ad23be575d0f9174c6114a137",
+    "url": "/static/media/dashboard2.1e23ef8a.png"
+  },
+  {
+    "revision": "6d99d805209128fa488e82e34b2e58ad",
+    "url": "/static/media/datamine.6d99d805.png"
+  },
+  {
+    "revision": "89369e321a913ac268e5a5a999b98924",
+    "url": "/static/media/fire_end.89369e32.gif"
+  },
+  {
+    "revision": "4e722fb49c8b3964055bebd34573584f",
+    "url": "/static/media/gamehive-battlerun.4e722fb4.png"
+  },
+  {
+    "revision": "aff8ea3e8e8eac3f042cdfa3bc88fdbd",
+    "url": "/static/media/graduate.aff8ea3e.svg"
+  },
+  {
+    "revision": "4fa5928047a36d3eae1f5c4415355aee",
+    "url": "/static/media/infographic-gamehive.4fa59280.png"
+  },
+  {
+    "revision": "17e6a63dec51251794c19f2013c8ded7",
+    "url": "/static/media/infographic-upsight.17e6a63d.png"
+  },
+  {
+    "revision": "4a5f1b599a5e0fc9b3d0fff21cc56952",
+    "url": "/static/media/newWeapons.4a5f1b59.jpg"
+  },
+  {
+    "revision": "d903b20e18ccfee987eaaade469a2107",
+    "url": "/static/media/old-cardwork.d903b20e.png"
+  },
+  {
+    "revision": "f3a8ec16beec4988df109e853b6c4b07",
+    "url": "/static/media/outlink.f3a8ec16.svg"
+  },
+  {
+    "revision": "e8befa3276c2b3c3890d3adae01836a7",
+    "url": "/static/media/revampExperience.e8befa32.jpg"
+  },
+  {
+    "revision": "98d970d72ee70eddda0f57a1cee6c76a",
+    "url": "/static/media/safe4work.98d970d7.jpg"
+  }
+]);
